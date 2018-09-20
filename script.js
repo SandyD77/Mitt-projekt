296dfe4d6aadb9d2d05fa664555e89107dc5af7a
SandyD77 <36622071+SandyD77@users.noreply.github.com>
2018-09-20 12:51:49 +0200

    Div småfix

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Our variables
 const key = "081464f2dd5a41ee8aaa8abb06632b8a"
-const url = `https://newsapi.org/v2/everything?language=sv&q=penningtvätt OR spelmyndigheten NOT musikal&from=2018-09-01S&sortBy=publishedAt&apiKey=${key}`
+const url = `https://newsapi.org/v2/everything?language=sv&q=penningtvätt OR spelmyndigheten NOT musikal&from=2018-09-04S&sortBy=publishedAt&apiKey=${key}`
+const url2 = `https://newsapi.org/v2/everything?language=sv&q=bedrägeri OR fraud&from=2018-09-10S&sortBy=publishedAt&apiKey=${key}`
 
 //const keyWeather = "e7d14d47781b029f604ee7b45f050e99"
 //const urlWeather = `http://api.openweathermap.org/data/2.5/forecast?id=2662689&APPID=${keyWeather}`
-
 
 // Our main function
 const recievedNews = (newsdata) => {
@@ -26,6 +26,28 @@ const recievedNews = (newsdata) => {
 fetch(url)
   .then(response => response.json())
   .then(recievedNews)
+
+//Fraud
+const recievedFraud = (frauddata) => {
+	// For each article object from the API, we create a new div in HTML.
+    frauddata.articles.forEach((article) => {
+					//Here we create and add html elements to our html file
+					document.querySelector(".allFraud").innerHTML +=
+            `<div class="fraud">
+            <img src="${article.urlToImage}"/>
+            <h2>${article.title}</h2>
+            <p>${article.description}</p>
+            <a href="${article.url}" target="_blank">Klicka här för att komma till nyheten!</a>
+            <p>Publicerad ${article.publishedAt}</p>
+            </div>`
+    })
+}
+
+//Fetch is a built in function in Javascript, it gets the data from the API and tranforms it into Javascript objects – JSON data.
+fetch(url2)
+  .then(response => response.json())
+  .then(recievedFraud)
+
 
   // When the user scrolls down 20px from the top of the document, show the button
   window.onscroll = function() {scrollFunction()};
